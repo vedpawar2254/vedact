f6c05b48f9996629324221386d041ce26ad6f1ec
Ved <vedpawar2254@gmail.com>
2025-06-21 18:51:14 +0530

    created react-core

---
+++ createElement.js
@@ -0,0 +1,42 @@
+const REACT_ELEMENT_TYPE = Symbol.for('react.element');
+
+export default function createElement(type, config, ...children) {
+  
+  let propName;
+  let props = {};
+  let key = null;
+  let ref = null;
+
+  if (config != null) {
+    if (config.ref !== undefined) {
+      ref = config.ref;
+    }
+    if (config.key !== undefined) {
+      key = '' + config.key;
+    }
+
+   
+    for (propName in config) {
+      if (propName !== 'key' && propName !== 'ref' && Object.hasOwnProperty.call(config, propName)) {
+        props[propName] = config[propName];
+      }
+    }
+  }
+
+  
+  if (children.length === 1) {
+    props.children = children[0];
+  } else if (children.length > 1) {
+    props.children = children;
+  }
+
+  
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type: type,
+    key: key,
+    ref: ref,
+    props: props,
+    _owner: null,
+  };
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/** @jsx createElement */
+import createElement from '../createElement.js';
+
+const el = (
+<div>
+    <h1>hi</h1>
+</div>
+);
+
+console.log(el);
